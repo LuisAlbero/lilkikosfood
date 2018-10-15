fcf44769b3e16802203f6c7a2819afdf4f85e0cc
LuisAlberto <al221611737@gmail.com>
2018-10-14 23:46:33 -0600

    Se creo la carpeta que llevara todas las vistas, tambien la vida adeuaca de cliente

---
+++ resources/views/sistemfood/altacliente.blade.php
@@ -0,0 +1,51 @@
+<html>
+    <body>
+    <h1>Alta de maestros</h1>
+        <form action ="{{route('guardamaestro')}}" method = 'POST' enctype='multipart/form-data'>
+        {{csrf_field()}}
+ 
+        @if($errors->first('idm')) 
+        <i> {{ $errors->first('idm') }} </i> 
+        @endif	<br>
+            
+        Clave <input type = 'text' name = 'idm' value="{{$idms}}" readonly = 'readonly'>
+        <br>
+        @if($errors->first('nombre')) 
+        <i> {{ $errors->first('nombre') }} </i> 
+        @endif	<br>
+
+        Nombre<input type = 'text' name = 'nombre' value="{{old('nombre')}}">
+        <br>
+        @if($errors->first('edad')) 
+        <i> {{ $errors->first('edad') }} </i> 
+        @endif	<br>
+
+        Edad<input type ='text' name = 'edad' value="{{old('edad')}}">
+        <br>
+        @if($errors->first('correo')) 
+        <i> {{ $errors->first('correo') }} </i> 
+        @endif	<br>
+
+        Correo<input type = 'text' name= 'correo' value="{{old('correo')}}">
+        <br>
+        @if($errors->first('cp')) 
+        <i> {{ $errors->first('cp') }} </i> 
+        @endif	<br>
+
+        Codigo Postal<input type = 'text' name= 'cp' value="{{old('cp')}}" alt='el codigo postal debe de ser de 5 digitos'>
+        <br><br>
+
+        Selecione carrera <select name = 'idc'>
+        @foreach ($carreras as $cr)
+        <option value = '{{$cr->idc}}'>{{$cr->nombre}}</option>
+        @endforeach</select>
+        <br><br>
+        @if($errors->first('archivo'))
+        <i>{{ $errors->first('archivo')}}</i>
+        @endif<br>
+        
+        Seleccione foto <input type='file' name='archivo'>
+        <input type = 'submit' value = 'Guardar'>
+        </form>
+    </body>
+</html>
